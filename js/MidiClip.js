cc767c51eaa3802af15c73f251e43a53481111ab
Pierrick Varin <pierrick.varin@glitchstudio.com>
2015-12-27 03:16:20 +0100

    Base Style and UI implementation for MIDI Clips Editor dialog

--- js/MidiClip.js
+++ js/MidiClip.js
@@ -29,6 +29,37 @@ var MidiClip = function(start, duration) {
 		this.notes.push( new MidiNote(note, start, duration) );
 	}
 
+	this.launchEditor = function(beatWidth) {
+		var midiRowsHTML = '<table class="midi-clip-editor-table"><tbody>';
+		var rows = [];
+		for(var noteName in Harmony.nameToFreq) {
+			var rowHTML = '<tr data-note="'+noteName+'" class="'+(noteName.indexOf('#') !== -1 ? 'sharp' : '')+'">';
+			rowHTML += '<th><span>'+noteName+'</span></th>';
+			for(var beat = 0; beat < this.duration; beat++) {
+				rowHTML += '<td style="width: '+beatWidth+'px;" class="'+(beat % 4 == 0 ? 'bar': '')+'"></td>';	
+			}
+			rowHTML += '</tr>';
+			rows.push(rowHTML);
+		}
+		rows.reverse();
+		midiRowsHTML += rows.join('') + '</tbody></table>';
+
+		var $editor = $('.midi-clip-editor');
+		$editor.html( midiRowsHTML ).dialog('open');
+		$editor.scrollTop( $editor.find('[data-note="A4"]').offset().top );
+	}
+
+	// Setup listeners
+	var that = this;
+	this.$dom.on('dblclick', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		
+		that.launchEditor(20);
+
+		return false;
+	});
+
 	// Boot
 	this.setStart(start);
 	this.setDuration(duration);
--- js/app.js
+++ js/app.js
@@ -34,42 +34,54 @@ jQuery( function($) {
         var $this = $(this);
 
         var start = $this.attr('data-beat');
-        var duration = 4;
+        var duration = 16;
 
         seq.activeTrack.addMidiClip(start, duration);
         return false;
     });
     
-
-    //var midiWindow = new midiView($('.midi-view'), 8, 4);
-
-    $('.note').on('click', function(e) {
-        e.preventDefault();
-
-        var noteName = $(this).attr('data-note');
-
-        mySynth.noteOn( noteName );
-
-        ( function(noteName) {
-            setTimeout(function() {
-                mySynth.noteOff(noteName);
-            }, 500);
-        }) (noteName);
-
-        return false;
+    // Register Dialogs
+    $('.midi-clip-editor').dialog({
+        autoOpen: false,
+        maxHeight: 400,
+        maxWidth: 800,
+        minWidth: 400,
+        title: 'MIDI Clip Editor',
+        buttons: [{
+            text: "OK",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        }]
     });
 
-    $('.chord').on('click', function(e) {
-        e.preventDefault();
+    // $('.note').on('click', function(e) {
+    //     e.preventDefault();
 
-        mySynth.noteOn('A4');
-        mySynth.noteOn('C4');
-        mySynth.noteOn('E5');
+    //     var noteName = $(this).attr('data-note');
 
-        setTimeout(function() {
-            mySynth.noteOff();
-        }, 500);
+    //     mySynth.noteOn( noteName );
 
-        return false;
-    })
+    //     ( function(noteName) {
+    //         setTimeout(function() {
+    //             mySynth.noteOff(noteName);
+    //         }, 500);
+    //     }) (noteName);
+
+    //     return false;
+    // });
+
+    // $('.chord').on('click', function(e) {
+    //     e.preventDefault();
+
+    //     mySynth.noteOn('A4');
+    //     mySynth.noteOn('C4');
+    //     mySynth.noteOn('E5');
+
+    //     setTimeout(function() {
+    //         mySynth.noteOff();
+    //     }, 500);
+
+    //     return false;
+    // })
 });
